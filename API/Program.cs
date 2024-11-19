0ff3713039aaf796d5e4d30a684a1f088ed1aac7
Mathieu Aubin <maubin@enssat.fr>
2024-11-19 16:35:03 +0000

    All endpoints for users

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,19 @@
+using System;
+using Application.Areas.Identity.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") 
+    ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => 
+    options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
