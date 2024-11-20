8af6266e6b1168de04e6d13840920d6c7761f935
Mathieu Aubin <maubin@enssat.fr>
2024-11-20 15:44:32 +0000

    OrderController

---
+++ API/Controllers/OrderController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Application.Areas.Identity.Data;
+using Application.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get
+        [HttpGet]
+        public JsonResult Get(Guid cartId, int itemId)
+        {
+            var result = _context.CartItems.Find(cartId, itemId);
+            if (result == null) return new JsonResult(NotFound());
+
+            return new JsonResult(Ok(result));
+        }
+
+        // Delete
+        [HttpDelete]
+        public JsonResult Delete(Guid cartId, int itemId)
+        {
+            var result = _context.CartItems.Find(cartId, itemId);
+            if (result == null) return new JsonResult(NotFound());
+
+            _context.CartItems.Remove(result);
+            _context.SaveChanges();
+
+            return new JsonResult(NoContent());
+        }
+
+        // GetAll
+        [HttpGet()]
+        public JsonResult GetAll()
+        {
+            var result = _context.CartItems.ToList();
+
+            return new JsonResult(Ok(result));
+        }
+    }
+}
